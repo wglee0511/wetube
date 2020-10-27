04683bbb1c3c110157f92ac7e5ac50e8146e6eea
wglee0511 <ranazang@naver.com>
2020-10-27 18:50:30 +0900

    Update req, res server

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ function handleListening(){
     console.log(`Listening on : http://localHost:${PORT}`)
 }
 
+function handleHome(req,res) {
+    console.log(req);
+    res.send("Hello from home");
+
+}
+
+function handleProfile(req,res){
+    res.send("You are on my profile");
+}
+
+app.get("/",handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
