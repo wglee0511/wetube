c18da4f79b0fac0e12bc1259377c89dae403f33b
wglee0511 <ranazang@naver.com>
2020-10-27 20:04:05 +0900

    Update concept of middleware

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ var app = express();
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`Listening on : http://localHost:${PORT}`);
+const handleListening = () => 
+    console.log(`Listening on : http://localHost:${PORT}`);
 
-const handleHome = (req,res) => res.send("Hello from home");
+const handleHome = (req, res) => res.send("Hello from home");
 
-const handleProfile = (req,res) => res.send("You are on my profile");
+const handleProfile = (req, res) => res.send("You are on my profile");
 
-app.get("/",handleHome);
+const betweenHome = (req, res, next) => {
+    console.log("between");
+    next();
+};
+app.use(betweenHome);
+
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
