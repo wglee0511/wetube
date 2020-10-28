cf5a8a2a663aac97120974777e039d53134d96f9
wglee0511 <ranazang@naver.com>
2020-10-28 19:38:06 +0900

    update routes for divide URL

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
+import routes from "../routes";
 
 export const userRouter = express.Router();
+
+userRouter.get(routes.users, (req, res) => res.send("users"));
+userRouter.get(routes.userDetail, (req, res) => res.send("user detail"));
+userRouter.get(routes.editProfile, (req, res) => res.send("edit profile"));
+userRouter.get(routes.changePassward, (req, res) => res.send("change passward"));
 
 export default userRouter;
