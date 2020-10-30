8522d75cd112930b93b8f112b888b86b29a4bd44
wglee0511 <ranazang@naver.com>
2020-10-30 22:39:44 +0900

    divide get/post login and set id when id is exist

--- middlewares.js
+++ middlewares.js
@@ -3,6 +3,7 @@ import routes from "./routes";
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
+    res.locals.user = {isAuthenticated : true, id:1};
 
     next(); // couse this middleware is been between connection and routers
 };
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,7 +6,7 @@ export const userRouter = express.Router();
 
 userRouter.get(routes.changePassward, changePassward);
 userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail(), userDetail);
 
 
 export default userRouter;
