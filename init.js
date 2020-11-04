fa8fa846129f0761d9beec99684d2d9d38db7ee0
wglee0511 <ranazang@naver.com>
2020-11-04 19:38:27 +0900

    Set video model
    Can find any schema on the mongoose js

--- init.js
+++ init.js
@@ -4,6 +4,8 @@ import dotenv from"dotenv";
 
 dotenv.config();
 
+import "./models/Video";
+
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(` ✅ Listening on : http://localhost : ${PORT}`);
