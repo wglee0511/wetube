e6a073191c9fbe2b2c18c9b921e51d8ca75752e1
wglee0511 <ranazang@naver.com>
2020-11-09 21:38:11 +0900

    install extract-text-webpack-plugin@next and loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,35 @@
 const path = require("path");
+const Extract = require("extract-text-webpack-plugin");
 
+const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
+
+
 const config = {
     entry : ENTRY_FILE,
+    mode : MODE,
+    module : {
+        rules : [
+            {
+                test : /\.(scss)$/,
+                use : Extract.extract([
+                    {
+                        loader : "css-loader"
+                    },
+                    {
+                        loader : "postcss-loader"
+                    },
+                    {
+                        loader : "sass-loader"
+                    }
+                ])
+                //how to extract css text
+                
+            }
+        ]
+    },
     output : {
         path : OUTPUT_DIR,
         filename : "[name].[format]"
