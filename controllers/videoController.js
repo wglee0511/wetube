874d97eb880ff552005f10f86fc6c0e94ed7ab9a
wglee0511 <ranazang@naver.com>
2020-11-09 18:21:21 +0900

    How to get newest video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,7 +4,7 @@ import Video from "../models/Video";
 //global
 export const home = async (req, res) => {
     try{
-        const videos = await Video.find({});
+        const videos = await Video.find({}).sort({_id:-1});
     res.render("home",{pageTitle:"Home", videos});
     }catch (error) {
         console.log(error);
