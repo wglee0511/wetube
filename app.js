e8f8be5713ea847f3638e517c2f9426cfbb0be6b
wglee0511 <ranazang@naver.com>
2020-11-16 19:56:03 +0900

    Using connect-mongo for being login

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import mongoose from "mongoose";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -14,6 +16,8 @@ import { localsMiddleware } from "./middlewares";
 import "./passport";
 
 const app = express();
+
+const CookieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug"); // set(name, value) setting view engine -> pug
@@ -28,7 +32,8 @@ app.use(
     session({
     secret : process.env.COOKIE_SECRET,
     resave : true,
-    saveUninitialized : false
+    saveUninitialized : false,
+    store : new CookieStore({ mongooseConnection : mongoose.connection })
 })
 );
 app.use(passport.initialize());
