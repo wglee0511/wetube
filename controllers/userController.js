2fc76550dd11a8394374d9ae1e81b55944f05c05
wglee0511 <ranazang@naver.com>
2020-11-17 19:45:56 +0900

    complete userDetail page (error)

--- controllers/userController.js
+++ controllers/userController.js
@@ -83,7 +83,16 @@ export const getMe = (req, res) => {
     res.render("userDetail",{pageTitle:"User Detail" , user : req.user});//user = current user
 }
 
-export const userDetail = (req, res) => res.render("userDetail",{pageTitle:"User Detail"});
+export const userDetail = async (req, res) => {
+    const { params : id } = req;
+    try {
+
+        const user = await User.findById(id);
+        res.render("userDetail",{pageTitle:"User Detail", user});
+    } catch(error) {
+        res.redirect(routes.home);
+    }
+};
 
 export const editProfile = (req, res) => res.render("editProfile",{pageTitle:"Edit Profile"});
 export const changePassward = (req, res) => res.render("changePassward",{pageTitle:"Change Passward"});
