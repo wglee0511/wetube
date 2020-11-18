8702ff952f93287c71839ff94c55d6ab84bbac73
wglee0511 <ranazang@naver.com>
2020-11-18 19:11:14 +0900

    Change avatar

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { changePassword, getEditProfile, userDetail } from "../controllers/userController";
-import { onlyPrivate } from "../middlewares";
+import { changePassword, getEditProfile, postEditProfiloe, userDetail } from "../controllers/userController";
+import { onlyPrivate, uploadAvatar } from "../middlewares";
 import routes from "../routes";
 
 export const userRouter = express.Router();
 
-userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar,postEditProfiloe);
+
+userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
 
