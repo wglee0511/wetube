2d5077960739b0393b430fbabbdda85d18c51d51
wglee0511 <ranazang@naver.com>
2020-11-18 21:21:35 +0900

    Simple protect edit video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -72,7 +72,11 @@ export const getEditVideo = async(req, res) => {
     } = req;
     try{
         const video = await Video.findById(id);
-        res.render("editVideo",{pageTitle:`Edit ${video.title}`, video});
+        if(video.creator !== req.user.id){
+            throw Error();
+        } else {
+            res.render("editVideo",{pageTitle:`Edit ${video.title}`, video});
+        }
     }catch(error){
         res.redirect(routes.home);
     }
