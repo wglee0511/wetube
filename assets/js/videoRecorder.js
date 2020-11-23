85ad0b61507551e3de8030a51e03d8e501e60386
wglee0511 <ranazang@naver.com>
2020-11-23 20:57:43 +0900

    Setting videoRecord

---
+++ assets/js/videoRecorder.js
@@ -0,0 +1,27 @@
+const recorderContainer = document.getElementById("jsRecordContainer");
+const recordBtn = document.getElementById("jsRecordBtn");
+const videoPreview = document.getElementById("jsVideoPreview");
+
+const startRecording = async () => {
+    try{
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio:true,
+            video:true
+        });
+        videoPreview.srcObject = stream;
+        videoPreview.muted = true;
+        videoPreview.play();
+    }catch(error){
+        recordBtn.innerHTML = "💔 Cant Record";
+        recordBtn.removeEventListener("click", startRecording);
+       
+    }
+};
+
+function init(){
+    recorderContainer.addEventListener("click", startRecording);
+}
+
+if(recorderContainer){
+    init();
+}
