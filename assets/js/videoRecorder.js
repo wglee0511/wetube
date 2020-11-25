4f60c44cadbf630381173f80a023eedf1cd556d2
wglee0511 <ranazang@naver.com>
2020-11-25 12:20:33 +0900

    download video

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,36 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+let videoRecorder;
+
+const handleVideoDate = (event) => {
+    const { data : videoFile } = event;
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(videoFile);
+    link.download = "recorded.webm";
+    document.body.appendChild(link);
+    link.click();
+
+};
+
+const stopRecording = () => {
+    videoRecorder.stop();
+    videoRecorder.removeEventListener("click", stopRecording);
+    recordBtn.addEventListener("click", getVideo);
+    recordBtn.innerHTML = "Start Recording";
+
+};
+
+const startRecording = () => {
+    videoRecorder = new MediaRecorder(streamObject);
+    videoRecorder.start();
+    videoRecorder.addEventListener("dataavailable", handleVideoDate);
+    videoRecorder.addEventListener("click", stopRecording);
+};
+
+
+const getVideo = async () => {
     try{
         const stream = await navigator.mediaDevices.getUserMedia({
             audio:true,
@@ -11,15 +40,19 @@ const startRecording = async () => {
         videoPreview.srcObject = stream;
         videoPreview.muted = true;
         videoPreview.play();
+        recordBtn.innerHTML = "Stop Recording";
+        streamObject = stream;
+        startRecording(stream);
     }catch(error){
-        recordBtn.innerHTML = "💔 Cant Record";
-        recordBtn.removeEventListener("click", startRecording);
-       
+        recordBtn.innerHTML = "💔 Cant Record"; 
+    } finally {
+        recordBtn.removeEventListener("click", getVideo);
+        
     }
 };
 
 function init(){
-    recorderContainer.addEventListener("click", startRecording);
+    recorderContainer.addEventListener("click", getVideo);
 }
 
 if(recorderContainer){
