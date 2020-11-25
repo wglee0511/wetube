08175b7a48eb26aeccdcf22f7f5e9135e682ee6c
wglee0511 <ranazang@naver.com>
2020-11-25 14:51:43 +0900

    fake comment (like real time)

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,17 +1,33 @@
 import axios from "axios";
 
 const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
 
+const increaseNumber = () => {
+    commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+};
+
+const addComment = (comment) => {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerHTML = comment;
+    li.appendChild(span);
+    commentList.prepend(li);
+    increaseNumber();
+};
 const sendComment = async (comment) => {
     const videoId = window.location.href.split("/videos/")[1];
     const response = await axios({
         url : `/api/${videoId}/comment`,
         method : "POST",
         data : {
-            comment : comment
+            comment
         }
     });
-    console.log(response);
+    if(response.status === 200) {
+        addComment(comment);
+    }
 };
 
 const handleSubmit = (event) => {
@@ -28,4 +44,4 @@ function init () {
 
 if(addCommentForm){
     init();
-};
+}
